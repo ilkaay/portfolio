b762d90f5bdca423bab344531ad0f5534583c795
ilkaay <ilkaay.yuksel@gmail.com>
2021-11-08 19:50:35 +0300

    contact link, blog link and repository link added

--- src/views/main/Main.js
+++ src/views/main/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import HomeButton from "../../components/HomeButton";
 import Logo from "../../components/Logo";
@@ -25,6 +26,33 @@ const Container = styled.div`
   padding: 2rem;
 `;
 
+const Contact = styled.a`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 2rem;
+  right: calc(1rem + 2vw);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const BLOG = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  right: calc(1rem + 2vw);
+  transform: rotate(90deg) translate(-50%, -50%);
+  text-decoration: none;
+  z-index: 1;
+`;
+const REPOSITORY = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  left: calc(1rem + 2vw);
+  transform: translate(-50%, -50%) rotate(-90deg);
+  text-decoration: none;
+  z-index: 1;
+`;
 const Main = () => {
   return (
     <MainContainer>
@@ -32,6 +60,22 @@ const Main = () => {
         <HomeButton />
         <Logo />
         <SocialMediaIcons />
+
+        <Contact
+          rel='noreferrer'
+          target='_blank'
+          href='mailto:ilkaay.yuksel@gmail.com'
+        >
+          <h4>Say hi...</h4>
+        </Contact>
+
+        <BLOG to='/blog'>
+          <h3>Blog</h3>
+        </BLOG>
+
+        <REPOSITORY to='/repository'>
+          <h3>Repository</h3>
+        </REPOSITORY>
       </Container>
     </MainContainer>
   );

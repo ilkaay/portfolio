7c30e7fff3fd92527354face384f2e9d83682564
İlkay Yüksel <77669807+ilkaay@users.noreply.github.com>
2022-04-05 16:14:35 +0300

    about update

--- src/views/about/components/Description.js
+++ src/views/about/components/Description.js
@@ -26,15 +26,16 @@ const Description = () => {
     <Container>
       I am a 4th-grade computer engineering student at Eskişehir Osmangazi
       University.
+      <br /> <br /> I am passionate about software development and designing
+      websites. I taught myself about front-end development because of that
+      passion.
       <br /> <br />
-      I am passionate about software development and designing websites. I
-      taught myself about front-end development because of that passion. I
-      learned HTML, CSS, JS and after that, I learned VUE, React, Angular,
-      Typescript and GraphQL to develop more professional apps. I am still
-      teaching myself about front-end engineering and currently, I practice with
-      the technologies I have learned.
-      <br /> <br />I am looking for new things to learn, to be a better
+      In October 2021 I joined as a volunteer intern at Piton R&D. In November
+      2021, I left Piton R&D and I am currently working at Gusto as a software
       engineer.
+      <br /> <br />I am still teaching myself about software engineering and
+      currently, I practice with the technologies I have learned. I am looking
+      for new things to learn, to be a better engineer.
     </Container>
   );
 };
